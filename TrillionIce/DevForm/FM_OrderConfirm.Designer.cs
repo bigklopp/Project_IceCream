3c8e66118d593136ea5d77fef3327dee0e5d68e0
bigklopp <zmxncbv4@naver.com>
2021-05-31 14:29:58 +0900

    LHC OrderConfirm 완료

--- TrillionIce/DevForm/FM_OrderConfirm.Designer.cs
+++ TrillionIce/DevForm/FM_OrderConfirm.Designer.cs
@@ -29,13 +29,67 @@ namespace DevForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvGrid = new System.Windows.Forms.DataGridView();
+            this.btnOrderConfirm = new System.Windows.Forms.Button();
+            this.btnOrderCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvGrid
+            // 
+            this.dgvGrid.AllowUserToAddRows = false;
+            this.dgvGrid.AllowUserToDeleteRows = false;
+            this.dgvGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvGrid.Location = new System.Drawing.Point(0, 0);
+            this.dgvGrid.Name = "dgvGrid";
+            this.dgvGrid.RowHeadersWidth = 51;
+            this.dgvGrid.RowTemplate.Height = 27;
+            this.dgvGrid.Size = new System.Drawing.Size(800, 361);
+            this.dgvGrid.TabIndex = 0;
+            // 
+            // btnOrderConfirm
+            // 
+            this.btnOrderConfirm.Location = new System.Drawing.Point(451, 367);
+            this.btnOrderConfirm.Name = "btnOrderConfirm";
+            this.btnOrderConfirm.Size = new System.Drawing.Size(155, 54);
+            this.btnOrderConfirm.TabIndex = 1;
+            this.btnOrderConfirm.Text = "주문 확정";
+            this.btnOrderConfirm.UseVisualStyleBackColor = true;
+            this.btnOrderConfirm.Click += new System.EventHandler(this.btnOrderConfirm_Click);
+            // 
+            // btnOrderCancel
+            // 
+            this.btnOrderCancel.Location = new System.Drawing.Point(622, 367);
+            this.btnOrderCancel.Name = "btnOrderCancel";
+            this.btnOrderCancel.Size = new System.Drawing.Size(151, 54);
+            this.btnOrderCancel.TabIndex = 2;
+            this.btnOrderCancel.Text = "주문 취소";
+            this.btnOrderCancel.UseVisualStyleBackColor = true;
+            this.btnOrderCancel.Click += new System.EventHandler(this.btnOrderCancel_Click);
+            // 
+            // FM_OrderConfirm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnOrderCancel);
+            this.Controls.Add(this.btnOrderConfirm);
+            this.Controls.Add(this.dgvGrid);
+            this.Name = "FM_OrderConfirm";
+            this.Text = "주문 내역";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FM_OrderConfirm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrid)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvGrid;
+        private System.Windows.Forms.Button btnOrderConfirm;
+        private System.Windows.Forms.Button btnOrderCancel;
     }
 }
 
--- TrillionIce/DevForm/FM_OrderConfirm.cs
+++ TrillionIce/DevForm/FM_OrderConfirm.cs
@@ -16,5 +16,240 @@ namespace DevForm
         {
             InitializeComponent();
         }
+        public void Inquire()
+        {
+            DBHelper helper = new DBHelper(false);
+            try
+            {
+                string sUserID = string.Empty;
+                string sUserName = string.Empty;
+                DataTable DtTemp = new DataTable();
+                DtTemp = helper.FillTable("SP_ORDER_LHC_S1", CommandType.StoredProcedure);
+
+                if (DtTemp.Rows.Count == 0)
+                {
+                    dgvGrid.DataSource = null;
+                    MessageBox.Show("조회할 데이터가 없습니다.");
+                }
+                else
+                {
+                    dgvGrid.DataSource = DtTemp;
+                }
+                //dataGridView1.CurrentRow.Cells["USERID"].Value.ToString();
+                
+
+                dgvGrid.Columns["USERID"].HeaderText = "고객 ID";
+                dgvGrid.Columns["USERNAME"].HeaderText = "고객 이름";
+                dgvGrid.Columns["ITEMNAME"].HeaderText = "품목";
+                dgvGrid.Columns["QUANTITY"].HeaderText = "주문 수량";
+                dgvGrid.Columns["ORDERDATE"].HeaderText = "주문 일시";
+                dgvGrid.Columns["STOCK"].HeaderText = "재고";
+                dgvGrid.Columns["ORDERSEQ"].HeaderText = "주문 번호";
+                dgvGrid.Columns["ITEMCODE"].HeaderText = "품목 코드";
+
+                dgvGrid.Columns[4].Width = 200;
+
+                // 컬럼의 수정 여부를 지정한다. 
+                dgvGrid.Columns["USERID"].ReadOnly = true;
+                dgvGrid.Columns["USERNAME"].ReadOnly = true;
+                dgvGrid.Columns["ITEMNAME"].ReadOnly = true;
+                dgvGrid.Columns["QUANTITY"].ReadOnly = true;
+                dgvGrid.Columns["ORDERDATE"].ReadOnly = true;
+                dgvGrid.Columns["STOCK"].ReadOnly = true;
+                dgvGrid.Columns["ORDERSEQ"].ReadOnly = true;
+                dgvGrid.Columns["ITEMCODE"].ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("예외 발생" + ex);
+            }
+            finally
+            {
+                helper.Close();
+            }
+
+        }
+
+
+        public void OrderCancel()
+        
+        {
+
+                /*if (dataGridView1.Rows.Count == 0) return;
+                int iSelectIndex = dataGridView1.CurrentCell.RowIndex;
+                DataTable DtTemp = (DataTable)dataGridView1.DataSource;
+                DtTemp.Rows[iSelectIndex].Delete();*/ // 지금 데이터 그리드 뷰의 선택된 셀의 로우인덱스에 해당하는 열을 지움
+
+                // 아니 DataTable의 객체 DtTemp의 행을 하나 지웠는데 왜 dataGridView의 행이 지워지냐고 ㅅㅂ
+                // dgv의 데이터 소스를 DataTable의 객체로 선언하면 실시간 연동이 되는거냐?
+                // 표 상에서만 지워
+
+                if (dgvGrid.Rows.Count == 0) return;
+            if (MessageBox.Show("해당 구매 내역을 삭제하시겠습니까?", "데이터 삭제", MessageBoxButtons.YesNo)
+            == DialogResult.No) return;
+
+            string orderSeq = dgvGrid.CurrentRow.Cells["ORDERSEQ"].Value.ToString(); //선택된 셀
+            string userID = dgvGrid.CurrentRow.Cells["USERID"].Value.ToString(); //선택된 셀
+            string itemCode = dgvGrid.CurrentRow.Cells["ITEMCODE"].Value.ToString(); //선택된 셀
+            string orderDate = dgvGrid.CurrentRow.Cells["ORDERDATE"].Value.ToString(); //선택된 셀
+
+            DataTable dtTemp = (DataTable)dgvGrid.DataSource;
+                for (int i = 0; i < dtTemp.Rows.Count; i++)
+                {
+                    if (dtTemp.Rows[i].RowState == DataRowState.Deleted) continue; //지워진 상태이면 다음 행으로
+                    if (dtTemp.Rows[i][6].ToString() == orderSeq &&
+                        dtTemp.Rows[i][0].ToString() == userID &&
+                        dtTemp.Rows[i][7].ToString() == itemCode &&
+                        dtTemp.Rows[i][4].ToString() == orderDate) //선택된 셀의 UserID와 같으면
+                    {
+                    dtTemp.Rows[i].Delete();
+                        break;
+                    }
+                }
+
+            DataTable dtTempChange = ((DataTable)dgvGrid.DataSource).GetChanges();
+            if (dtTempChange == null) return;
+
+            DBHelper helper = new DBHelper(true);
+
+            try
+            {
+                // FOREACH : 내가 추가, 수정, 삭제한 모든 행동을 GETCHANGE로 담아서 모두 처리한다.
+
+                foreach (DataRow drRow in dtTempChange.Rows)
+                {
+                    if (drRow.RowState == DataRowState.Deleted)
+                    {
+                        drRow.RejectChanges();
+                        userID = drRow["USERID"].ToString();
+                        itemCode = drRow["ITEMCODE"].ToString();
+                        orderSeq = drRow["ORDERSEQ"].ToString();
+                        orderDate = drRow["ORDERDATE"].ToString();
+
+                        helper.ExecuteNoneQuery("SP_ORDER_LHC_D1",
+                            CommandType.StoredProcedure, helper.CreateParameter("USERID", userID)
+                            , helper.CreateParameter("ITEMCODE", itemCode)
+                            , helper.CreateParameter("ORDERSEQ", orderSeq)
+                            , helper.CreateParameter("ORDERDATE", orderDate));
+
+                    }
+                }
+                // 성공 시 DB Commit
+                helper.Commit();
+                // 메세지
+                MessageBox.Show("정상적으로 삭제하였습니다.");
+                // 재조회
+                Inquire();
+
+            }
+            catch (Exception ex)
+            {
+                helper.Rollback();
+
+                MessageBox.Show("데이터 삭제에 실패하였습니다." + ex);
+            }
+            finally
+            {
+                helper.Close();
+            }
+
+        }
+
+        public void OrderConfirm()
+        {
+            if (dgvGrid.Rows.Count == 0) return;
+            if (MessageBox.Show("해당 구매 내역을 확정하시겠습니까?", "데이터 적용", MessageBoxButtons.YesNo)
+            == DialogResult.No) return;
+
+            string orderSeq = dgvGrid.CurrentRow.Cells["ORDERSEQ"].Value.ToString(); //선택된 셀
+            string userID = dgvGrid.CurrentRow.Cells["USERID"].Value.ToString(); //선택된 셀
+            string itemCode = dgvGrid.CurrentRow.Cells["ITEMCODE"].Value.ToString(); //선택된 셀
+            string orderDate = dgvGrid.CurrentRow.Cells["ORDERDATE"].Value.ToString(); //선택된 셀
+
+            DataTable dtTemp = (DataTable)dgvGrid.DataSource;
+            for (int i = 0; i < dtTemp.Rows.Count; i++)
+            {
+                if (dtTemp.Rows[i].RowState == DataRowState.Deleted) continue; //지워진 상태이면 다음 행으로
+                if (dtTemp.Rows[i][6].ToString() == orderSeq &&
+                    dtTemp.Rows[i][0].ToString() == userID &&
+                    dtTemp.Rows[i][7].ToString() == itemCode &&
+                    dtTemp.Rows[i][4].ToString() == orderDate) //선택된 셀의 UserID와 같으면
+                {
+                    dtTemp.Rows[i].Delete();
+                    break;
+                }
+            }
+
+
+            string userName = string.Empty;
+            string passWord = string.Empty;
+            int quantity;
+
+            DataTable dtTempChange = ((DataTable)dgvGrid.DataSource).GetChanges();
+            if (dtTempChange == null) return;
+
+            DBHelper helper = new DBHelper(true);
+
+            try
+            {
+                // FOREACH : 내가 추가, 수정, 삭제한 모든 행동을 GETCHANGE로 담아서 모두 처리한다.
+                foreach (DataRow drRow in dtTempChange.Rows)
+                { 
+                    if (drRow.RowState == DataRowState.Deleted)
+                        {
+                            drRow.RejectChanges();
+                            userID = drRow["USERID"].ToString();
+                            itemCode = drRow["ITEMCODE"].ToString();
+                            orderSeq = drRow["ORDERSEQ"].ToString();
+                            orderDate = drRow["ORDERDATE"].ToString();
+                            quantity = int.Parse(drRow["QUANTITY"].ToString());
+                            helper.ExecuteNoneQuery("SP_ORDER_LHC_U1", CommandType.StoredProcedure
+                                , helper.CreateParameter("USERID", userID)
+                                , helper.CreateParameter("ITEMCODE", itemCode)
+                                , helper.CreateParameter("ORDERSEQ", orderSeq)
+                                , helper.CreateParameter("ORDERDATE", orderDate)
+                                , helper.CreateParameter("QUANTITY", quantity));
+                        }
+                }
+                // 성공 시 DB Commit
+                helper.Commit();
+                // 메세지
+                MessageBox.Show("정상적으로 반영하였습니다.");
+                // 재조회
+                Inquire();
+
+            }
+            catch (Exception ex)
+            {
+                helper.Rollback();
+
+                MessageBox.Show("주문 확정에 실패하였습니다." + ex);
+            }
+            finally
+            {
+                helper.Close();
+            }
+
+
+        }
+       
+
+        private void FM_OrderConfirm_Load(object sender, EventArgs e)
+        {
+            Inquire();
+        }
+
+
+
+        private void btnOrderConfirm_Click(object sender, EventArgs e)
+        {
+
+            OrderConfirm();
+        }
+
+        private void btnOrderCancel_Click(object sender, EventArgs e)
+        {
+            OrderCancel();
+        }
     }
 }
